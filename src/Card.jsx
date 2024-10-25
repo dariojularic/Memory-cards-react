5f71688ad55d128abee9665b0da14bfc7df312b0
Dario Jularic <dariojularic0@gmail.com>
2024-10-25 17:25:00 +0200

    Refactor code

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,37 +1,19 @@
-import {useEffect, useState} from "react"
 import "./Card.css"
 import PropTypes from "prop-types";
 
 function Card(props) {
-  // const [isClicked, setIsClicked] = useState(cards)
-  // console.log("props", props)
-  const {id, image, name, isClicked, handleGameOver, shuffleArray, handleClickCard, currentScore, setCurrentScore, highScore, setHighScore} = props;
-  // console.log("isClicked",isClicked)
-
-  // function handleClick()  {
-  //   if(!isClicked) {
-  //     setIsClicked(true);
-  //   } else {
-  //     handleGameOver();
-  //   }
-  // }
+  const {id, image, name, isClicked, handleGameOver, shuffleArray, handleClickCard, setCurrentScore} = props;
 
   return (
     <li className="card" onClick={() => {
-      // console.log(isClicked)
       if (isClicked === false) {
-        // console.log("gagaag")
-        setCurrentScore((currentScore) => currentScore + 1)
+        setCurrentScore((prev) => prev + 1)
         shuffleArray()
         handleClickCard(id)
-        // setIsClicked(true)
-        // console.log("current score", currentScore)
-        // console.log(highScore)
-        // currentScore > highScore ? setHighScore(currentScore) : console.log("currentScore je manji od highScore")
       }
       else {
-        console.log("isClicked je true")
-        // handleGameOver()
+        // console.log("isClicked je true")
+        handleGameOver()
       }
     }}>
       <img className="character-image" src={`public/${image}.png`} alt="" />
