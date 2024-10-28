aeb8acc482f784df5e6f1b744dad859c26068301
Dario Jularic <dariojularic0@gmail.com>
2024-10-28 15:21:22 +0100

    Add style

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,9 @@ function App() {
       return {...card, isClicked: false}
     })
     setMyCards(resetCards)
-    setCurrentScore(0)
     setOverlayVisibility(true)
+    setCurrentScore(0)
+    // console.log("ovaj radi", currentScore)
     // alert("game over")
   }
 
@@ -44,37 +45,33 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <header>
-        <h1>Rick and Morty memory game</h1>
-      </header>
+    <>
+      <div className="app">
+        <header>
+          <h1>Rick and Morty memory game</h1>
+        </header>
 
-      <main>
-        <div className="score">
-          <p className="current-score">Current Score: {currentScore}</p>
-          <p className="high-score">High Score: {highScore}</p>
-        </div>
-        <ul className="cards-grid">
-          {myCards.map(card => {
-            return <Card key={card.id} {...card} handleGameOver={handleGameOver} handleClickCard={handleClickCard} setCurrentScore={setCurrentScore}/>
-          })}
-        </ul>
-      </main>
-
-
+        <main>
+          <div className="score">
+            <p className="current-score">Current Score: {currentScore}</p>
+            <p className="high-score">High Score: {highScore}</p>
+          </div>
+          <ul className="cards-grid">
+            {myCards.map(card => {
+              return <Card key={card.id} {...card} handleGameOver={handleGameOver} handleClickCard={handleClickCard} setCurrentScore={setCurrentScore}/>
+            })}
+          </ul>
+        </main>
+      </div>
       {overlayVisibility &&
         <Modal>
-            <h3>Game Over!</h3>
-            <p className="score-result">{currentScore}</p>
+            <h3 className="game-over-header">Game Over!</h3>
+            {console.log("ne radi", currentScore)}
+            <p className="score-result">Your score: {currentScore}</p>
             <button className="play-again-btn" onClick={() => setOverlayVisibility(false)}>Play again</button>
         </Modal>
       }
-      {/* <div className="game-over">
-        <h3>Game Over!</h3>
-        <p className="score-result">{currentScore}</p>
-        <button className="play-again-btn">Play again</button>
-      </div> */}
-    </div>
+    </>
   )
 }
 
